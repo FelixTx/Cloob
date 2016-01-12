b0bd6e672b1edcc78bfa1da6a9682f104db3f7c6
FelixTx <felix.tournoux@gmail.com>
2016-01-12 17:29:19 +0100

    code couleur
    divers test

--- Cloobi/app/src/main/java/fr/turfu/cloobi/MainActivity.java
+++ Cloobi/app/src/main/java/fr/turfu/cloobi/MainActivity.java
@@ -2,12 +2,15 @@ package fr.turfu.cloobi;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.osmdroid.api.IGeoPoint;
 import org.osmdroid.util.ResourceProxyImpl;
 import org.osmdroid.DefaultResourceProxyImpl;
 import org.osmdroid.ResourceProxy;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.Projection;
 import org.osmdroid.views.overlay.  ItemizedIconOverlay;
 import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
 import org.osmdroid.views.overlay.OverlayItem;
@@ -20,13 +23,16 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 
@@ -48,11 +54,18 @@ public class MainActivity extends Activity {
         //Activer le zoom et le tactile
         myOpenMapView.setBuiltInZoomControls(true);
         myOpenMapView.setMultiTouchControls(true);
-
         myMapController = (MapController) myOpenMapView.getController();
         myMapController.setZoom(15);
 
-
+// GET THE MARKERS IN SCREEN AREA =================== THIS SHIT WONT WORK :(:(:(:(:(:(:(:(
+   /*    IGeoPoint ct = myOpenMapView.getMapCenter();
+        float res[]= new float[]{0};
+        IGeoPoint ct2= new GeoPoint(47.237358, -1.545327);
+        IGeoPoint monpt= new GeoPoint(47.237212, -1.547687);
+        Location.distanceBetween(ct2.getLatitude(), ct2.getLatitude(), monpt.getLatitude(), monpt.getLongitude(), res);
+        float disp=res[0];
+        String d = Float.toString(disp);
+*/
         //--- Create Overlay
         overlayItemArray = new ArrayList<OverlayItem>();
 // Il serait pratique d'utiliser un custom resource proxy, mais c'est galere
@@ -69,28 +82,37 @@ public class MainActivity extends Activity {
         // Liste des icones
         ArrayList<OverlayItem> items = new ArrayList<OverlayItem>();
         // on crée chaque element i
-        OverlayItem i1 =new OverlayItem("Tramway", "Nike la Tan", new GeoPoint(47.22, -1.55));
+        OverlayItem i1 =new OverlayItem("C'est la tan !", "coucou", new GeoPoint(47.22, -1.55));
         // On crée un item i2 a partir d'un element de la classe Station
         GeoPoint g=new GeoPoint(47.215, -1.55);
-        Station s=new TStation((GeoPoint)g);
+        Station s=new TStation(g);
         // on génere i2 à partir de s
-        OverlayItem i2 =new OverlayItem(s.nom, s.toString(), s.pos);
+        //OverlayItem i2 =new OverlayItem(s.nom, s.toString(), s.pos);
+        OverlayItem i2 =new OverlayItem("caca","whatever", s.pos);
         // On crée un objet d'image "drawable", pour lui associer le fichier png "R.drawable.tan"
-        Drawable icon_t = this.getResources().getDrawable(R.drawable.tan);
+      /*  Drawable icon_t = this.getResources().getDrawable(R.drawable.tan);
         // on l'associe aux Items Bicloos
         i1.setMarker(icon_t);
-        i2.setMarker(icon_t);
-        // On ajoute les items a la liste
+        i2.setMarker(icon_t);*/
+        // On ajoute les itemrs a la liste
         items.add(i1);
         items.add(i2);
 //On inclut la liste des icones dans un overlay
+
+        //ItemizedOverlayWithFocus(final List<Item> aList, final Drawable pMarker,
+       // final Drawable pMarkerFocused, final int pFocusedBackgroundColor,
+      //  final ItemizedIconOverlay.OnItemGestureListener<Item> aOnItemTapListener, final ResourceProxy pResourceProxy)
+
+        Drawable tan = this.getResources().getDrawable(R.drawable.tan);
+        Drawable tan_foc = this.getResources().getDrawable(R.drawable.tan_foc);
+        int col=Color.rgb(50,110,180);
 // (we can change default color in itemizedoverlaywithfocus.java)
-        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(items,
+            ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(items, tan, tan_foc, col,
                 new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
                     @Override
                     public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
-                        //do something
-                        return true;
+                        // CHARGER LES JHORAIRES DE LA TAN DANS LACTIVITE BANDEAU EN HAUT ?
+                        return false;
                     }
                     @Override
                     public boolean onItemLongPress(final int index, final OverlayItem item) {
@@ -105,20 +127,29 @@ public class MainActivity extends Activity {
 
 
         // TEST BICLOO ===================================== =============================
-
         //IDEM AS TAN
+        int orange=Color.rgb(245,180,0);
         ArrayList<OverlayItem> items2 = new ArrayList<OverlayItem>();
         GeoPoint g2=new GeoPoint(47.21, -1.555);
-        Station s2=new BStation((GeoPoint)g2);
+        Station s2=new BStation(g2);
         OverlayItem it1 =new OverlayItem(s2.nom, s2.toString(), s2.pos);
-        OverlayItem it2 =new OverlayItem("5", "Station bicloo", "7 / 100", new GeoPoint(47.215, -1.56));
-        Drawable icon_b = this.getResources().getDrawable(R.drawable.bic);
-        it1.setMarker(icon_b);
-        it2.setMarker(icon_b);
+        OverlayItem it2 =new OverlayItem("5", "Station quasi vide", "2 / 100", new GeoPoint(47.215, -1.56));
+        OverlayItem it3 =new OverlayItem("Station bicloo pleine", "100 / 100", new GeoPoint(47.21, -1.56));
+        OverlayItem it4 =new OverlayItem("Station vide", "0 / 100", new GeoPoint(47.215, -1.555));
+        Drawable bic = this.getResources().getDrawable(R.drawable.bic);
+        Drawable bic3 = this.getResources().getDrawable(R.drawable.bic3);
+        Drawable bic_full = this.getResources().getDrawable(R.drawable.bic_full);
+        Drawable bic_empty = this.getResources().getDrawable(R.drawable.bic_empty);
+        it1.setMarker(bic);
+        it2.setMarker(bic3);
+        it3.setMarker(bic_full);
+        it4.setMarker(bic_empty);
         items2.add(it1); // Lat/Lon decimal degrees
         items2.add(it2);
+        items2.add(it3);
+        items2.add(it4);
 //the overlay (we can change default color in itemizedoverlaywithfocus.java)
-        ItemizedOverlayWithFocus<OverlayItem> mOverlay2 = new ItemizedOverlayWithFocus<OverlayItem>(items2,
+        ItemizedOverlayWithFocus<OverlayItem> mOverlay2 = new ItemizedOverlayWithFocus<OverlayItem>(items2,bic,bic,orange,
                 new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
                     @Override
                     public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
